b260dae126c22f16e2b66af8976c34be59e88ce7
Aaron Hooper <rnjckhpr@gmail.com>
2019-12-31 22:39:42 +0000

    Add service methods

--- src/app/sc-rest.service.ts
+++ src/app/sc-rest.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,10 +8,43 @@ import { Observable } from 'rxjs';
 export class ScRestService {
 
   private url = 'https://jsonplaceholder.typicode.com/posts';
+  private keyPath = 'assets/api-key.txt';
+  private base = 'http://api.soundcloud.com';
+  private resolveUrl = this.base + '/resolve';
 
   constructor(private http: HttpClient) { }
 
   getJson(): Observable<any> {
     return this.http.get(this.url);
+  }
+
+  getFavorites(url: string, key: string) {
+    const target = this.createFavoritesUrl(url);
+    const params = new HttpParams()
+      .set('limit', '200')
+      .set('linked_partitioning', '1')
+      .set('client_id', key);
+
+    return this.http.get(target, { params }).toPromise();
+  }
+
+  createFavoritesUrl(url: string) {
+    return url + '/favorites';
+  }
+
+  async resolve(url: string, key: string) {
+    const params = new HttpParams()
+      .set('url', url)
+      .set('client_id', key);
+
+    const json = await this.http.get(this.resolveUrl, { params })
+      .toPromise();
+    return json.uri;
+  }
+
+  getKey() {
+    return this.http.get(this.keyPath, { responseType: 'text' })
+      .toPromise()
+      .then(text => text.trim());
   }
 }
