881d0fef4899341958b5dfbc3fcb357fc2dd4c75
Aaron Hooper <rnjckhpr@gmail.com>
2019-12-31 22:41:07 +0000

    Define component methods for service usage

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,13 +8,31 @@ import { ScRestService } from '../sc-rest.service';
 })
 export class SearchComponent implements OnInit {
   searchText: string;
-  json: object[];
+  json: any;
+  key: string;
 
   constructor(private scRest: ScRestService) { }
 
   ngOnInit() {
     this.scRest.getJson()
       .subscribe(data => this.json = data);
+
+    this.scRest.getKey()
+      .then(key => this.key = key);
+  }
+
+  async getFavorites() {
+    const resolved = await this.scRest.resolve(this.searchText, this.key);
+    this.json = await this.scRest.getFavorites(resolved, this.key);
+    console.log(this.json);
+
+    this.downloadFile(this.json);
+  }
+
+  downloadFile(data) {
+    const blob = new Blob([data], { type: 'text/json' });
+    const url = window.URL.createObjectURL(blob);
+    window.open(url);
   }
 
 }
