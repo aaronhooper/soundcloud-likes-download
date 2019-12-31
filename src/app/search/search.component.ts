dfebe3d59cf2e903f3214e79b3157df7018929f6
Aaron Hooper <rnjckhpr@gmail.com>
2019-12-31 01:28:06 +0000

    Subscribe to observable from injected service

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScRestService } from '../sc-rest.service';
 
 @Component({
   selector: 'app-search',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
   searchText: string;
+  json: object[];
 
-  constructor() { }
+  constructor(private scRest: ScRestService) { }
 
   ngOnInit() {
+    this.scRest.getJson()
+      .subscribe(data => this.json = data);
   }
 
 }
